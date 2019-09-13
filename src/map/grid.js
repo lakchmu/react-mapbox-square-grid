e22319d8f604c051aa7af254ac47676498f89e23
Дарья Курмаева <lakchmu@gmail.com>
2019-09-13 21:46:08 +0600

    Use multi polygon for grid

--- src/map/grid.js
+++ src/map/grid.js
@@ -7,26 +7,46 @@ export default function addGrid(map, upperRight, bottomLeft, cellSide) {
   const options = { units: 'miles' };
 
   const sg = squareGrid(bbox, cellSide, options);
+  console.log(map.addLayer);
 
-  sg.features.forEach((feature, index) => {
-    map.addLayer({
-      'id': `${index}`,
-      'type': 'fill',
-      'source': {
-        'type': 'geojson',
-        'data': {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Polygon',
-            'coordinates': feature.geometry.coordinates
-          }
-        }
-      },
-      'layout': {},
-      'paint': {
-        'fill-color': `${getColor(index)}`,
-        'fill-opacity': 0.8
-      }
-    });
+  map.addSource("grid", {
+    "type": "geojson",
+    "data": sg
   });
+
+  map.addLayer({
+    "id": "grid",
+    "type": "fill",
+    "source": "grid",
+    "paint": {
+      'fill-outline-color': '#fff',
+      'fill-color': 'transparent',
+      'fill-opacity': 0.8
+    },
+    "filter": ["==", "$type", "Polygon"]
+  });
+
+  // sg.features.forEach((feature, index) => {
+  //   map.addLayer({
+  //     'id': `${index}`,
+  //     'type': 'fill',
+  //     'source': {
+  //       'type': 'geojson',
+  //       'data': {
+  //         'type': 'Feature',
+  //         'geometry': {
+  //           'type': 'Polygon',
+  //           'coordinates': feature.geometry.coordinates
+  //         }
+  //       }
+  //     },
+  //     'layout': {},
+  //     'paint': {
+  //       'fill-outline-color': '#fff',
+  //       // 'fill-color': `${getColor(index)}`,
+  //       'fill-color': 'transparent',
+  //       'fill-opacity': 0.8
+  //     }
+  //   });
+  // });
 }
